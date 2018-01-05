ee8fe03585333659fad022c60d3e8f779dd71378
Vuk Đuranović <vuk.djuranovic@udg.edu.me>
2018-01-05 18:26:12 +0100

    3rd atempt

--- program.js
+++ program.js
@@ -3,8 +3,8 @@ var mongo = require('mongodb').MongoClient;
 require("dotenv").config();
 var url =`mongodb://${process.env.USERNAME}:${process.env.PASSWOURD}@ds125914.mlab.com:25914/urls`;
 var app = express();
-app.get("/", function(request, response)){
+app.get("/", function(request, response){
 	var obj = { "user" : process.env.USERNAME, "pass" : process.env.PASSWOURD };
 	response.send(obj);
-}
+});
 app.listen(process.env.PORT || 5000);
